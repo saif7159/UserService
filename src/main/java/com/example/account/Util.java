ac3544ee57d6a095e3914c56281e24436c53c509
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2020-05-13 17:20:57 +0530

    Update Util.java

--- src/main/java/com/example/account/Util.java
+++ src/main/java/com/example/account/Util.java
@@ -22,9 +22,11 @@ public class Util {
     private static final String SESS_PASS = "SESSION_PASS";
     private static final String SESS_TYPE = "SESSION_TYPE";
 
+
     @Autowired
     public UserDaoRepository userdao;
 
+//    Creating token
     public String createToken(String sess_email, String sess_pass, String sess_type) {
         Map<String, Object> map = new HashMap<>();
         map.put(SESS_EMAIL, sess_email);
@@ -32,19 +34,46 @@ public class Util {
         map.put(SESS_TYPE, sess_type);
 
         SignatureAlgorithm algo = SignatureAlgorithm.HS512;
-        String br = Jwts.builder().setIssuer(ISSUER).setClaims(map).setSubject(SUBJECT)
+
+
+        return Jwts.builder().setIssuer(ISSUER).setClaims(map).setSubject(SUBJECT)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000*60*60*24))
                 .signWith(algo, Key).compact();
-        return br;
     }
 
-    public User checkToken(String token)
-    {
-        Claims claims = Jwts.parser().setSigningKey(Key).parseClaimsJws(token).getBody();
-        User user = userdao.findByEmailAndPasswordAndUserPermission(claims.get(SESS_EMAIL).toString(),
-                claims.get(SESS_PASS).toString(), claims.get(SESS_TYPE).toString());
+//    Verifying token
+    public User checkToken(String token) {
+        if(token!=null && token.startsWith("Bearer "))
+            token = token.substring(7);
+        User user = null;
+        System.out.println("token: "+token);
+        try {
+            Claims claims = Jwts.parser().setSigningKey(Key).parseClaimsJws(token).getBody();
+            user = userdao.findByEmailAndPasswordAndUserPermission(claims.get(SESS_EMAIL).toString(),
+                    claims.get(SESS_PASS).toString(), claims.get(SESS_TYPE).toString());
+        } catch (Exception e) {
+            System.out.println("no user found");
+        }
         return user;
     }
 
-}
+//    Check Expiration date
+    public boolean isTokenExpired(String token) {
+        if(token!=null && token.startsWith("Bearer "))
+            token = token.substring(7);
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
 
+    private Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser().setSigningKey(Key).parseClaimsJws(token).getBody();
+        return claimsResolver.apply(claims);
+    }
+
+}
 
